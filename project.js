dcaf33a91f26bd4925a9da6af9dc0f37ddc194b6
Thierry Nowak <thinow@gmail.com>
2014-01-07 13:33:06 +0100

    Authentificate with manual refresh

--- project.js
+++ project.js
@@ -2,8 +2,23 @@ angular.module('project', ['firebase'])
 
 	.constant('REMOTE_SERVER', 'https://sandbox-auth.firebaseio.com')
 
-	.controller('MainController', ['$scope', 'REMOTE_SERVER', function ($scope, REMOTE_SERVER) {
-	
-		$scope.output = 'Hello Controller !!!'
-	
+	.controller('MainController', ['$scope', 'REMOTE_SERVER', '$firebaseAuth', function ($scope, REMOTE_SERVER, $firebaseAuth) {
+
+		var auth = $firebaseAuth(new Firebase(REMOTE_SERVER));
+
+		$scope.login = function() {
+			auth.$login('password', {
+				  email: 'mail@mail.com',
+				  password: 'password'
+			});
+		};
+
+		$scope.logout = function() {
+			auth.$logout();
+		};
+
+		$scope.refresh = function() {
+			$scope.output = auth.user;
+		};
+
 	}]);
