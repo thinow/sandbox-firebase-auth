684aad9236271d4827b9ed2760c3e22e1c28f9e0
Thierry Nowak <thinow@gmail.com>
2014-01-07 13:45:54 +0100

    Add log

--- project.js
+++ project.js
@@ -2,15 +2,17 @@ angular.module('project', ['firebase'])
 
 	.constant('REMOTE_SERVER', 'https://sandbox-auth.firebaseio.com')
 
-	.controller('MainController', ['$scope', 'REMOTE_SERVER', '$firebaseAuth', function ($scope, REMOTE_SERVER, $firebaseAuth) {
+	.controller('MainController', ['$scope', 'REMOTE_SERVER', '$firebaseAuth', '$log', function ($scope, REMOTE_SERVER, $firebaseAuth, $log) {
 
 		var auth = $firebaseAuth(new Firebase(REMOTE_SERVER), {
 			'callback' : function() {
+				$log.info('Refresh : ' + (auth.user != null ? auth.user.email : '(no-user)'));
 				$scope.output = auth.user;
 			}
 		});
 
 		$scope.login = function() {
+			$log.info('Login');
 			auth.$login('password', {
 				  email: 'mail@mail.com',
 				  password: 'password'
@@ -18,6 +20,7 @@ angular.module('project', ['firebase'])
 		};
 
 		$scope.logout = function() {
+			$log.info('Logout');
 			auth.$logout();
 		};
 
